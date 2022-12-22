8c567d49b11ad30d194fa15ec8a78d50093b19aa
vvknyazev <knyazev.vlad03@gmail.com>
2022-12-22 14:00:57 +0200

    add menu logo action

--- js/main/script.js
+++ js/main/script.js
@@ -93,6 +93,7 @@ let content = document.querySelector(".content");
 const header = document.querySelector(".header");
 const menuBody = document.querySelector(".menu__body");
 const veil = document.querySelector(".veil");
+const logo = document.querySelector(".logo");
 
 function closeMenu() {
     content.classList.remove("blur");
@@ -109,16 +110,18 @@ function openMenu() {
     // document.body.classList.add("ovflow");
 }
 
-
 btnMenu.addEventListener("click", openMenu);
 menuBack.addEventListener("click", closeMenu);
 veil.addEventListener("click", closeMenu);
+logo.addEventListener("click", closeMenu);
 
 window.addEventListener("keyup", function (e) {
     if (e.key == 'Escape') {
         closeMenu();
     }
 })
+
+
 
 
 
